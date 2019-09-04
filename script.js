2a986cffb7ddfbb1554cfaecf70fd65daf8d124e
Sylwester <sylwesterf@users.noreply.github.com>
2019-09-04 17:28:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function drawLineChart() {
 
   var jsonData = $.ajax({
-    url: 'https://cors.io/?https://s3.eu-central-1.amazonaws.com/pms3003/data.json',
+    url: 'https://s3.eu-central-1.amazonaws.com/pms3003/data.json',
     dataType: 'json',
   }).done(function (results) {
 
